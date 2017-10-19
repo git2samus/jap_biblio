7dc7927c684ecea92a0ae841c0e47e576f18bd9d
Michael Cetrulo <mail2samus@gmail.com>
2017-10-19 10:14:12 -0300

    campo "prestado" en libro

--- web/models.py
+++ web/models.py
@@ -6,6 +6,8 @@ class Libro(models.Model):
     titulo = models.CharField(max_length=255)
     isbn = models.CharField(max_length=13)
 
+    prestado = models.BooleanField()
+
     def __str__(self):
         return "Libro <{} ({})>".format(self.titulo, self.isbn)
 
