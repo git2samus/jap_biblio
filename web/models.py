1837e98d65be2d86e3c347e45b293a13ac579508
Michael Cetrulo <mail2samus@gmail.com>
2017-10-26 10:30:38 -0300

    migrar campo prestamo

--- web/models.py
+++ web/models.py
@@ -1,12 +1,14 @@
 from django.db import models
 from django.urls import reverse
 
+from django.contrib.auth.models import User
+
 
 class Libro(models.Model):
     titulo = models.CharField(max_length=255)
     isbn = models.CharField(max_length=13)
 
-    prestado = models.BooleanField()
+    prestado = models.ForeignKey(User, null=True, blank=True)
     fecha_devolucion = models.DateTimeField(null=True, blank=True)
 
     def __str__(self):
